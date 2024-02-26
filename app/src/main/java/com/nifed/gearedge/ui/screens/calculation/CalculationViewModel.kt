d74e1fcd5140986525e7de92b7bc0af7435aed86
nife <nife0612@gmail.com>
2024-02-26 18:21:28 +0400

    A little rework

--- app/src/main/java/com/nifed/gearedge/ui/screens/calculation/CalculationViewModel.kt
+++ app/src/main/java/com/nifed/gearedge/ui/screens/calculation/CalculationViewModel.kt
@@ -6,6 +6,9 @@ import com.nifed.gearedge.ui.screens.calculation.state.ButtonCalculationEvent
 import com.nifed.gearedge.ui.screens.calculation.state.CalculationEvent
 import com.nifed.gearedge.ui.screens.calculation.state.CalculationState
 
+// the const for  private fun calculateResult(size: String, feedRate: String): String
+const val SCALE_RATE = 5.6
+const val BASIC_FEED_RATE = 20.0
 class CalculationViewModel: ViewModel() {
     var calculationState = mutableStateOf(CalculationState())
         private set
@@ -19,13 +22,12 @@ class CalculationViewModel: ViewModel() {
                 calculationState.value = calculationState.value.copy(
                     size = calculationEvent.inputValue
                 )
-                this.onUiEvent(calculationEvent = CalculationEvent.TimeChanged(""))
+
             }
             is CalculationEvent.FeedRateChanged -> {
                 calculationState.value = calculationState.value.copy(
                     feedRate = calculationEvent.inputValue
                 )
-                this.onUiEvent(calculationEvent = CalculationEvent.TimeChanged(""))
             }
 
 
@@ -45,47 +47,49 @@ class CalculationViewModel: ViewModel() {
                 calculationState.value = calculationState.value.copy(
                     size = shiftValue(calculationState.value.size, calculationEvent.value)
                 )
-                this.onUiEvent(calculationEvent = CalculationEvent.TimeChanged(""))
             }
             is ButtonCalculationEvent.DecreaseSize -> {
                 calculationState.value = calculationState.value.copy(
                     size = shiftValue(calculationState.value.size, calculationEvent.value)
                 )
-                this.onUiEvent(calculationEvent = CalculationEvent.TimeChanged(""))
             }
             is ButtonCalculationEvent.IncreaseFeedRate -> {
                 calculationState.value = calculationState.value.copy(
                     feedRate = shiftValue(calculationState.value.feedRate, calculationEvent.value)
                 )
-                this.onUiEvent(calculationEvent = CalculationEvent.TimeChanged(""))
             }
             is ButtonCalculationEvent.DecreaseFeedRate -> {
                 calculationState.value = calculationState.value.copy(
                     feedRate = shiftValue(calculationState.value.feedRate, calculationEvent.value)
                 )
-                this.onUiEvent(calculationEvent = CalculationEvent.TimeChanged(""))
             }
         }
+
+        this.onUiEvent(calculationEvent = CalculationEvent.TimeChanged(""))
     }
 
 
 
 
-    private fun calculateResult(size: String, feedRate: String = "20.0"): String{
-        val scaleRate = 5.6
-        val sizeDB: Double = if(size.isNotBlank()) size.toDouble() else 0.0
-        val feedRateDB: Double = if(feedRate.isNotBlank()) feedRate.toDouble() else 0.0
+    private fun calculateResult(size: String, feedRate: String): String{
+        val sizeDB: Double = convertStringToDouble(size)
+        val feedRateDB: Double = convertStringToDouble(feedRate)
 
 
-        val requiredTime = if(feedRateDB == 0.0) sizeDB / (20.0 * scaleRate)
-            else sizeDB / (feedRateDB * scaleRate)
+        val requiredTime = if(feedRateDB == 0.0) sizeDB / (BASIC_FEED_RATE * SCALE_RATE)
+            else sizeDB / (feedRateDB * SCALE_RATE)
         return String.format("%.2f", requiredTime)
     }
 
+    private fun convertStringToDouble(value: String): Double{
+        return if(value.toDoubleOrNull() != null) value.toDouble() else 0.0
+    }
+
+
     private fun shiftValue(value: String, delta: Double = 1.0): String{
         val valueDB: Double =
-            if(value.isNotBlank())
-                if (value.toDouble() >= 0.0) value.toDouble() else 0.0
+            if(value.toDoubleOrNull() != null)
+                if (value.toDouble() > 0.0) value.toDouble() else 0.0
             else 0.0
 
         return String.format("%.2f", valueDB + delta)
